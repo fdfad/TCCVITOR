c547383de086ca77fc43c23b1f900624a6e83d5a
fdfad <vitorxr1000@gmail.com>
2023-11-04 11:45:57 -0300

    front end

--- TCCIA.py
+++ TCCIA.py
@@ -2,153 +2,111 @@ import pandas as pd
 import time
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pygame
+import sys
+from pygame.locals import *
 
+pygame.init()
+
+#Cria a tabela
 tabela = pd.read_csv("Alunos.csv", index_col=0)
 
-tabela.isnull().sum()
+materias = ["matematica", "biologia", "portugues", "fisica", "quimica", "filosofia", "sociologia", "geografia", "historia", "ingles"]
+
+#Filtra as informaçoes da tabela
+for i in range(9):
+    tabela[materias[i]] = pd.to_numeric(tabela[materias[i]], downcast="float")
+
+#Configurações pygame
+Tela = pygame.display.set_mode((800, 700))
+fonte = pygame.font.SysFont('Roboto', 40, True, False)
+clock = pygame.time.Clock()
+pygame.display.set_caption("Analise das suas notas em 6 anos")
+
+#Criação dos botões
+ProximoIMG = pygame.image.load("button_proximo.png").convert_alpha()
+VoltarIMG = pygame.image.load("button_voltar.png").convert_alpha()
+GraficosIMG = pygame.image.load("button_graficos.png").convert_alpha()
+
+#Button class
+class Button():
+    def __init__(self,x,y,image,scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, ((int(width * scale)) ,(int(height * scale))))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.clicked = False
+        
+    def draw(self):
+        action = False
+        
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        Tela.blit(self.image,(self.rect.x,self.rect.y))
+        
+        return action
+    
+Proximo = Button(500,500,ProximoIMG,1)
+Voltar = Button(100,500,VoltarIMG,1)    
+Graficos = Button(100,600,GraficosIMG,1)  
+
+i = 0
+
+while True:
+    clock.tick(60)
+    
+    Tela.fill((225, 229, 242))
+    
+    maior = str(tabela[materias[i]].dropna().max())
+    menor = str(tabela[materias[i]].dropna().min())
+    media = str(tabela[materias[i]].dropna().mean())
+    Materia = str(materias[i])
+    
+    texto = fonte.render("A sua nota media em "+ str(materias[i]) +" foi " + maior, True, (0,0,0))
+    texto2 = fonte.render("A sua menor nota em "+ str(materias[i]) +" foi " + menor, True, (0,0,0))
+    texto3 = fonte.render("A sua nota media em "+ str(materias[i]) +" foi " + media, True, (0,0,0))
+    texto4 = fonte.render(Materia, True, (0,0,0))
+    
+    Tela.blit(texto, (100, 150))
+    Tela.blit(texto2, (100, 250))
+    Tela.blit(texto3, (100, 350))
+    Tela.blit(texto4, (300, 50))
+    
+    if Proximo.draw():
+        if i != 9:
+            i += 1
+        
+    if Voltar.draw():
+        if i != 0:
+            i -= 1
+        
+    if Graficos.draw():
+        print(tabela.describe())
+        tabela.dropna().plot.area(figsize=(18, 8), subplots=True)
+        tabela.dropna().hist()
+        plt.show()
+        
+    
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    pygame.display.update()
+    
 
 
-tabela["mat"] = pd.to_numeric(tabela["mat"], downcast="float")
-tabela["bio"] = pd.to_numeric(tabela["bio"], downcast="float")
-tabela["port"] = pd.to_numeric(tabela["port"], downcast="float")
-tabela["fisica"] = pd.to_numeric(tabela["fisica"], downcast="float")
-tabela["quimica"] = pd.to_numeric(tabela["quimica"], downcast="float")
-tabela["filosofia"] = pd.to_numeric(tabela["filosofia"], downcast="float")
-tabela["sociologia"] = pd.to_numeric(tabela["sociologia"], downcast="float")
-tabela["geo"] = pd.to_numeric(tabela["geo"], downcast="float")
-tabela["historia"] = pd.to_numeric(tabela["historia"], downcast="float")
-tabela["ingles"] = pd.to_numeric(tabela["ingles"], downcast="float")
 
 
 
-
-
-
-
-
-plt.show()
-print("Analise dos seu desempenho escolar em 6 anos")
-time.sleep(1)
-
-
-############ MATEMATICA #############
-
-print("A sua maior nota em matematica foi " + str(tabela["mat"].dropna().max()))
-time.sleep(1)
-
-print("A sua menor nota em matematica foi " + str(tabela["mat"].dropna().min()))
-time.sleep(1)
-
-print("A sua nota media em matematica foi " + str(tabela["mat"].dropna().mean()))
-time.sleep(1)
-
-############ BIOLOGIA #############
-
-print("A sua maior nota em biologia foi " + str(tabela["bio"].dropna().max()))
-time.sleep(1)
-
-print("A sua menor nota em biologia foi " + str(tabela["bio"].dropna().min()))
-time.sleep(1)   
-
-print("A sua nota media em biologia foi " + str(tabela["bio"].dropna().mean()))
-time.sleep(1)
-
-############ Português #############
-
-print("A sua maior nota em português foi " + str(tabela["port"].dropna().max()))
-time.sleep(1)
-
-print("A sua menor nota em português foi " + str(tabela["port"].dropna().min()))
-time.sleep(1)
-
-print("A sua nota media em português foi " + str(tabela["port"].dropna().mean()))
-time.sleep(1)
-
-############ FÍSICA #############
-
-print("A sua maior nota em física foi " + str(tabela["fisica"].dropna().max()))
-time.sleep(1)
-
-print("A sua menor nota em física foi " + str(tabela["fisica"].dropna().min()))
-time.sleep(1)
-
-print("A sua nota media em física foi " + str(tabela["fisica"].dropna().mean()))
-time.sleep(1)
-
-############ QUIMICA #############
-
-print("A sua maior nota em quimica foi " + str(tabela["quimica"].dropna().max()))
-time.sleep(1)
-
-print("A sua menor nota em quimica foi " + str(tabela["quimica"].dropna().min()))
-time.sleep(1)
-
-print("A sua nota media em quimica foi " + str(tabela["quimica"].dropna().mean()))
-time.sleep(1)
-
-############ FILOSOFIA #############
-
-print("A sua maior nota em filosofia foi " + str(tabela["filosofia"].dropna().max()))
-time.sleep(1)
-
-print("A sua menor nota em filosofia foi " + str(tabela["filosofia"].dropna().min()))
-time.sleep(1)
-
-print("A sua nota media em filosofia foi " + str(tabela["filosofia"].dropna().mean()))
-time.sleep(1)
-
-############ SOCIOLOGIA #############
-
-print("A sua maior nota em sociologia foi " + str(tabela["sociologia"].dropna().max()))
-time.sleep(1)
-
-print("A sua menor nota em sociologia foi " + str(tabela["sociologia"].dropna().min()))
-time.sleep(1)
-
-print("A sua nota media em sociologia foi " + str(tabela["sociologia"].dropna().mean()))
-time.sleep(1)
-
-############ GEOGRAFIA #############
-
-print("A sua maior nota em geografia foi " + str(tabela["geo"].dropna().max()))
-time.sleep(1)
-
-print("A sua menor nota em geografia foi " + str(tabela["geo"].dropna().min()))
-time.sleep(1)
-
-print("A sua nota media em geografia foi " + str(tabela["geo"].dropna().mean()))
-time.sleep(1)
-
-############ INGLES #############
-
-print("A sua maior nota em ingles foi " + str(tabela["ingles"].dropna().max()))
-time.sleep(1)
-
-print("A sua menor nota em ingles foi " + str(tabela["ingles"].dropna().min()))
-time.sleep(1)
-
-print("A sua nota media em ingles foi " + str(tabela["ingles"].dropna().mean()))
-time.sleep(1)
-
-############ HISTORIA #############
-
-print("A sua maior nota em historia foi " + str(tabela["historia"].dropna().max()))
-time.sleep(1)
-
-print("A sua menor nota em historia foi " + str(tabela["historia"].dropna().min()))
-time.sleep(1)
-
-print("A sua nota media em historia foi " + str(tabela["historia"].dropna().mean()))
-time.sleep(1)
-
-print(tabela.describe())
-
-print("Aqui estão alguns graficos para melhor visualização do seu desempenho")
-time.sleep(1)
-
-
-tabela.dropna().hist()
-tabela.dropna().plot.area(figsize=(18, 8), subplots=True)
-
-plt.show()
 
